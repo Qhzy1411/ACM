247828ff0486e76e41c0e01b5c4bee92a066a518
青荷芷烟 <jinshicheng06@gmail.com>
2025-06-14 21:46:35 +0800

    2025/06/14 21:46

---
+++ CPP/test.cpp
@@ -0,0 +1,14 @@
+// Author: QHZY
+// Create_Time: 2025/06/14 21:45:04
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const ll N = 1e6;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << "Hello world!";
+    return 0;
+}
