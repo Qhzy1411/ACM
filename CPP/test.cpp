dce7930420745689e1fe884da73296641b799991
青荷芷烟 <jinshicheng06@gmail.com>
2025-07-17 09:09:12 +0800

    2025/07/17 09:09

--- CPP/test.cpp
+++ CPP/test.cpp
@@ -1,14 +1,22 @@
 // Author: QHZY
-// Create_Time: 2025/06/14 21:45:04
+// Create_Time: 2025/07/17 09:08:00
 #include <bits/stdc++.h>
-#define ll long long
+using ll = long long;
+using ull = unsigned long long;
+#define int ll
 using namespace std;
-const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3f;
 const ll N = 1e6;
-int main() {
+void work(){
+    
+}
+signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cout << "Hello world!";
+    cout.tie(0);
+    int _ = 1;
+    cin >> _;
+    while (_--)
+        work();
     return 0;
 }
